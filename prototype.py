256628e1d2b79e051d264d1084e5ae9958e73cd4
Pasha Anpilogov <homiache@gmail.com>
2022-03-08 23:14:21 +0300

    Rough prototype is created

--- prototype.py
+++ prototype.py
@@ -2,11 +2,39 @@
 
 import re
 import nltk  # nltk is used for count distance between two texts.
+import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+# First primitive config info for bot. Contains intents info and phrases for failure cases.
+# Intents is like classes for phrases. Every phrase can be classified, e. g. "Hello", "Hi", "Good afternoon"
+# or "Good morning" are greetings.
+# TODO move to the separate file
+BOT_CONFIG = {
+    "intents": {
+        "hello": {
+            "examples": [
+                "Hello", "Hi", "Good afternoon", "Good morning", "Hey", "Yo", "Good evening",
+                "Nice to meet you", "Pleased to meet you", "Yoyo", ],
+            "responses": ["Hi", "Hello", "Good to see you", "Nice to see you"],
+        },
+        "bye": {
+            "examples": ["Goodbye", "Bye", "Bye Bye", "See you later", "Seeya", "See you", "I’m off", "Stay safe"],
+            "responses": ["Goodbye", "Bye", "See you later"]
+        },
+        "how_are_you": {
+            "examples": ["Are you OK", "You alright", "Alright mate", "Howdy", "Sup", "Whazzup"],
+            "responses": ["Awesome", "Fantastic", "I’m fine", "Not bad"],
+        },
+    },
+    "failure_phrases": ["I do not understand. Try again."]
+}
 
 
 def normalize(text):
     """
-    Function removes punctuations from a given string and returns the string in lowercase.
+    The function removes punctuation from the given string and returns a lowercase string.
     :param text: string
     :return: string
     """
@@ -36,7 +64,7 @@ def is_matching(text1, text2):
     text1 = normalize(text1)
     text2 = normalize(text2)
 
-    # Count of unmatched chars aka distance in ML things. :)
+    # Count of unmatched chars aka "distance" in ML things. :)
     distance = nltk.edit_distance(text1, text2)
 
     # Get average length of texts
@@ -44,4 +72,117 @@ def is_matching(text1, text2):
 
     # Count percents and return a result
     # TODO Move 0.4 to the config file
+    # TODO process ZeroDivisionError
     return distance / average_length < 0.4
+
+
+def get_intent(text):
+    """
+    Figure out intentions.
+    :param text: string, user input
+    :return: string, intent name
+    """
+
+    # Check all examples of intents, trying to find a match.
+    all_intents = BOT_CONFIG["intents"]
+    for intent_name, data in all_intents.items():
+        for example in data["examples"]:
+            if is_matching(text, example):
+                return intent_name
+
+    # Return None if intent is not found
+    return None
+
+
+def get_answer(intent):
+    """
+    Returns random answer for a given intent
+    :param intent: string, intent_name
+    :return: string, random answer
+    """
+
+    # TODO process invalid value
+    responses = BOT_CONFIG["intents"][intent]["responses"]
+    return random.choice(responses)
+
+
+def create_model():
+    """
+    TODO More description
+    TODO Move to a separate script because creation of a model is a rare thing I hope.
+    TODO Store already prepared model in a separate file.
+    Creates a ML model. The task of the model is to learn how to find intent "y" by input example "x".
+    :return: sklearn.feature_extraction.text.CountVectorizer and sklearn.linear_model._logistic.LogisticRegression
+    """
+
+    # Examples.
+    x = []
+
+    # Classes.
+    y = []
+
+    # Create two lists of examples and intents.
+    for name, data in BOT_CONFIG["intents"].items():
+        for example in data['examples']:
+
+            # Get all examples in an one list x.
+            x.append(example)
+
+            # Get all intent names as classes in an one list y.
+            y.append(name)
+
+    # Creating an object that can transform a set of examples into a set of vectors of numbers
+    vectorizer = CountVectorizer()
+
+    # Pass a list of examples so that the vectorizer analyzes them
+    vectorizer.fit(x)
+
+    # Transform examples into vectors (sets of numbers)
+    x_vectorized = vectorizer.transform(x)
+
+    # Create an object of logistic regression model.
+    model = LogisticRegression()
+
+    # Teach the model.
+    # After that the model can predict an intent (return predict name) for a given input example.
+    model.fit(x_vectorized, y)
+
+    return vectorizer, model
+
+
+def bot(text):
+    """
+    The bot actually. Get an input as a text and decides what to do.
+    :param text: string
+    :return: None
+    """
+
+    # Try to find indent
+    intent = get_intent(text)
+
+    # If intent is not found try to predict it.
+    # Model returns a list of one element with the predicted intent name, so get the first element.
+    if not intent:
+        intent = model.predict(vectorizer.transform([text]))[0]
+
+    print("(Intent is {})".format(intent))
+
+    # If we have an intent then return a response
+    if intent:
+        return get_answer(intent)
+
+    # Mock
+    # TODO Need to understand if this code will ever be executed. Seems like model never gives up.
+    failure_phrases = BOT_CONFIG['failure_phrases']
+    return random.choice(failure_phrases)
+
+
+# Just for rough testing
+if __name__ == '__main__':
+
+    # Create the vectorizer and model objects before running the bot
+    vectorizer, model = create_model()
+
+    # Unleash the beast!
+    while True:
+        print(bot(input()))
